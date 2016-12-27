32c43daf51a8bb11cc2ce92dc204237f111a3324
m4b3l <mabel.yes@hotmail.com>
2016-12-26 21:35:25 -0400

    added additional pages

---
+++ test/Models/expression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test.Models
+{
+    public class Expression
+    {
+        public string ImgPath { get; set; }
+        public string description { get; set; }
+
+    }
+}
--- test/Scripts/Custom/landingPage.js
+++ test/Scripts/Custom/landingPage.js
@@ -1,4 +1,30 @@
-﻿
+﻿var localStream;
+
+var apiConnection = function () {
+    var pageUrl = "Home/Detect";
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var data = canvas.toDataURL("image/jpeg");
+    
+    var parameters = { "photo" : data};
+    
+    $.ajax({
+        type: "POST",
+        url: pageUrl,
+        data: JSON.stringify(parameters),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (data) {
+            return true;
+        },
+        error: function (data, success, error) {
+            alert("Error : " + error);
+        }
+    });
+
+    return false;
+}
+
 var startBouncing = function () {
     setInterval(function () {
         $("#arrow").animate({ 'marginTop': '+=5em' });
@@ -16,23 +42,27 @@ var getCamera = function () {
         navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
             video.src = window.URL.createObjectURL(stream);
             video.play();
+            localStream = stream;
         });
     }
+}
 
-    /*
-    //Acceder al dispositivo de camara web para mostrar el video
-    navigator.getUserMedia({"video" : true}, 
-    //navigator.webkitGetUserMedia("video",
-        function (stream) {
-            //video.src = webkitURL.createObjectURL(stream);//Obtenemos el video fuente de nuestra eitqueta video para mostrarlo
-            video.src = stream;
-            video.play();
-        },
-        function (err) {
-            console.log("Unable to get video stream!");//obtenemos algun error posible al realizar esto
-        }
-    );
-    */
+var turnOffCamera = function () {
+    $('#TryExpress').text("Try it now!");
+    $('#StopExpress').prop('disabled', true);
+
+    var video = document.getElementById("live");
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        // Not adding `{ audio: true }` since we only want video now
+        //function (stream) {
+            video.src = '';
+            video.pause();
+            var curStream = localStream.getTracks()[0];
+            curStream.stop();
+            curStream = null;
+        //};
+    }
 }
 
 var buttonsManager = function () {
@@ -41,89 +71,22 @@ var buttonsManager = function () {
         if (buttonTryExpress.textContent == "Try it now!") {
             $('#TryExpress').text("ExpressIt");
             getCamera();
+            $('#StopExpress').prop('disabled', false);
         } else {
-            $('#divPopup').fadeIn();
+            $('#divPopup').modal('show');
             var canvas = document.getElementById('canvas');
             var context = canvas.getContext('2d');
             var video = document.getElementById('live');
-            context.drawImage(video, 0, 0, 300, 225);
-
+            if (apiConnection()){
+                context.drawImage(video, 0, 0, 300, 225);
+            }
         }
-    });
-}
-
-var activateAccordion = function(){
-    var icons = {
-        header: "ui-icon-circle-arrow-e",
-        activeHeader: "ui-icon-circle-arrow-s"
-    };
-    $("#accordion").accordion({
-        collapsible: true,
-        active: false,
-        header: "h3",
-    });
-    //$("#accordion").accordion("option", "icons", icons);
-}
-
-var resizeSideBar = function () {
-    var altura = $('.pure-content').height();
-    $('.sidebar').css({
-        'height': altura,
-    });
+    });    
 }
 
 var prepareDoc = function () {
-    $('#divPopup').fadeOut();
-    applySize();
     startBouncing();
     buttonsManager();
-    activateAccordion();
-    resizeSideBar();
 }
 
-$(document).click(function (event) {
-    if ($('#divPopup').is(":visible")) {
-        if (!$(event.target).closest('#divPopup').length) {
-            $('#divPopup').fadeOut();
-        }
-    }
-});
-
-var applySize = function () {
-    var wHeight = $(window).height();
-    var barHeight = $('.navbar').height();
-    var footerHeight = $('.footer').height();
-    var finalHeight = wHeight - barHeight;
-    var downHeight = finalHeight - footerHeight - 5;
-    $('.pagePart').css({
-        'height': finalHeight + 'px'
-    });
-    $('.carousel-inner').css({
-        'height': finalHeight + 'px'
-    });
-    $('.pagePartDown').css({
-        'height': downHeight + 'px'
-    });
-    $('body').css({
-        'padding-top': barHeight + 'px',
-    });
-    var curBodySize = Math.max($('.pure-content').height() - 2, downHeight);
-    $('.onlyPagePart').css({
-        'height': curBodySize + 'px'
-    });
-    resizeSideBar();
-};
-
-/*
-$(':not(#divPopup)').click(function () {
-    $('#divPopup').fadeOut();
-});
-/*
-$(document).click(function (e) {
-    if ($(e.target).closest('#divPopup').length != 0) return false;
-    $('#divPopup').fadeOut();
-});
-*/
 $('document').ready(prepareDoc);
-
-$(window).resize(applySize);
